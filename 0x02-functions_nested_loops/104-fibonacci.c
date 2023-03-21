a8831e2bd16c2bcbc9887c3f3d9e0a77d4f19826
mahmoud-elbanna <melbanna964@gmail.com>
2023-03-21 14:50:44 -0700

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 * main - Prints the first 98 Fibonacci numbers, separated by comma and space
 *
@@ -11,38 +10,29 @@ int count;
 unsigned long int prev = 1;
 unsigned long int curr = 2;
 unsigned long int next;
-
 printf("%lu, %lu", prev, curr);
-
 for (count = 3; count <= 98; count++)
 {
 next = prev + curr;
 prev = curr;
 curr = next;
-
 printf(", %lu", next);
 }
-
 printf("\n");
-
 /*Calculate the sum of even-valued terms in the sequence*/
 unsigned long int sum = 0;
 prev = 1;
 curr = 2;
-
 while (curr <= 4000000)
 {
 if (curr % 2 == 0)
 {
 sum += curr;
 }
-
 next = prev + curr;
 prev = curr;
 curr = next;
 }
-
 printf("Sum of even-valued terms: %lu\n", sum);
-
 return (0);
 }
